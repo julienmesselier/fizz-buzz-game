63a68f02b15055a0d0c72a6bb5e9a6eac5ab44c3
julien <julien.messelier@sgcib.com>
2017-11-11 18:38:09 +0100

    tests as spec

--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -1,36 +1,33 @@
-from unittest import TestCase
-from fizz_buzz import *
+import unittest
+import fizz_buzz as fb
 
 
-class TestFizz_buzz(TestCase):
+class TestEvaluate(unittest.TestCase):
+    def test_multiple_of_3_should_return_Fizz(self):
+        self.assertEqual('Fizz', fb.evaluate(6))
+
+    def test_multiple_of_5_should_return_Fizz(self):
+        self.assertEqual('Buzz', fb.evaluate(10))
+
+    def test_multiple_of_3_and_5_should_return_Fizz(self):
+        self.assertEqual('FizzBuzz', fb.evaluate(15))
+
+    def test_other_ways(self):
+        self.assertEqual('8', fb.evaluate(8))
+
+
+class TestFizz_buzz(unittest.TestCase):
     def test_elements_count_should_be_100(self):
-        self.assertEqual(100, len(fizz_buzz()))
-
-    def test_1st_element_should_be_1(self):
-        self.assertEqual('1', fizz_buzz()[0])
-
-    def test_2nd_element_should_be_2(self):
-        self.assertEqual('2', fizz_buzz()[1])
-
-    def test_3rd_element_should_be_Fizz(self):
-        self.assertEqual('Fizz', fizz_buzz()[2])
-
-    def test_4th_element_should_be_4(self):
-        self.assertEqual('4', fizz_buzz()[3])
-
-    def test_5th_element_should_be_Buzz(self):
-        self.assertEqual('Buzz', fizz_buzz()[4])
-
-    def test_15th_element_should_be_FizzBuzz(self):
-        self.assertEqual('FizzBuzz', fizz_buzz()[14])
+        self.assertEqual(100, len(fb.fizz_buzz_list()))
 
 
-class TestFizz_buzz_variations(TestCase):
+class TestFizz_buzz_variations(unittest.TestCase):
     def test_variation_1_should_return_same_as_original_implementation(self):
-        self.assertEqual(fizz_buzz_variation_1(), fizz_buzz())
+        self.assertEqual(fb.fizz_buzz_variation_1(), fb.fizz_buzz_list())
 
     def test_variation_2_should_return_same_as_original_implementation(self):
-        self.assertEqual(fizz_buzz_variation_2(), fizz_buzz())
+        self.assertEqual(fb.fizz_buzz_variation_2(), fb.fizz_buzz_list())
 
-    def test_variation_3_should_return_same_as_original_implementation(self):
-        self.assertEqual(fizz_buzz_variation_3(), fizz_buzz())
+
+if __name__ == '__main__':
+    unittest.main()
