0fed22e37c783d32c35e217f41e86441adf2fec3
julien <julien.messelier@sgcib.com>
2017-11-11 18:44:26 +0100

    refactor

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -8,11 +8,12 @@ def evaluate(n):
     """
     if not n % 3 and not n % 5:
         return 'FizzBuzz'
-    if not n % 3:
+    elif not n % 3:
         return 'Fizz'
-    if not n % 5:
+    elif not n % 5:
         return 'Buzz'
-    return str(n)
+    else:
+        return str(n)
 
 
 def fizz_buzz_list():
